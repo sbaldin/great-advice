2db7e90ac2d4166ae7f5e92a9a91fe790be76d8f
Sergey Baldin <fesswoodwork@gmail.com>
2022-11-13 14:42:56 +0400

    Advice-grpc: setup base http application

--- advice-grpc/src/main/kotlin/com/github/sbaldin/greatadvice/Application.kt
+++ advice-grpc/src/main/kotlin/com/github/sbaldin/greatadvice/Application.kt
@@ -1,15 +1,11 @@
 package com.github.sbaldin.greatadvice
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.github.sbaldin.greatadvice.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
+    configureSerialization()
 }
