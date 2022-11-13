a2e6143721c4d3f65d448c128e32ba9fa0bf7bb1
Sergey Baldin <fesswoodwork@gmail.com>
2022-11-13 14:42:16 +0400

    Advice-core: added serialization plugin

--- advice-core/src/main/kotlin/com/github/sbaldin/greatadvice/domain/Models.kt
+++ advice-core/src/main/kotlin/com/github/sbaldin/greatadvice/domain/Models.kt
@@ -1,22 +1,24 @@
 package com.github.sbaldin.greatadvice.domain
 
+import kotlinx.serialization.Serializable
+
+
 data class GreatAdviceResponse(
     val status: String,
     val errors: List<String>,
     val data: List<GreatAdvice>
 )
 
-
+@Serializable
 data class GreatAdvice constructor(
     val id: String,
     val text: String,
     val html: String,
     val tags: List<String>,
     val conclusions: List<GreatAdviceConclusion>
-) {
+)
 
-}
-
+@Serializable
 data class GreatAdviceConclusion (
     val id: String,
     val text: String,
